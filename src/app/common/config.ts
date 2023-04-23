159ae62c0ee1aa3e6a43efc1e37445015e2c52db
LQYld <54627537+LQYld@users.noreply.github.com>
2023-04-23 22:10:56 +0800

    feat: add All app

---
+++ src/app/common/config.ts
@@ -0,0 +1,129 @@
+export const Installed_config = {
+  expanded: [
+    {
+      label: 'GitHub Desktop',
+      icon: '',
+      introduction: '',
+      download: {
+        windows:
+          'https://desktop.githubusercontent.com/github-desktop/releases/3.2.1-f3241b2b/GitHubDesktopSetup-x64.exe',
+        macOS:
+          'https://desktop.githubusercontent.com/github-desktop/releases/3.2.1-f3241b2b/GitHubDesktop-x64.zip'
+      }
+    },
+    {
+      label: 'Sourcetree',
+      icon: '',
+      introduction: '',
+      download: {
+        windows:
+          'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-3.4.12.exe',
+        macOS:
+          'https://product-downloads.atlassian.com/software/sourcetree/ga/Sourcetree_4.2.2_250.zip'
+      }
+    },
+    {
+      label: 'nvm',
+      icon: '',
+      introduction: '',
+      download: {
+        windows: 'https://github.com/coreybutler/nvm-windows/releases',
+        macOS: 'https://github.com/coreybutler/nvm-windows/releases'
+      }
+    },
+    {
+      label: 'PotPlayer',
+      icon: '',
+      introduction: '',
+      download: {
+        windows: 'https://potplayer.org/',
+        macOS: 'https://potplayer.org/'
+      }
+    },
+    {
+      label: 'Bandizip',
+      icon: '',
+      introduction: '',
+      download: {
+        windows: 'https://cn.bandisoft.com/bandizip/dl.php?old/',
+        macOS: 'https://cn.bandisoft.com/bandizip/dl.php?old/'
+      }
+    },
+    {
+      label: 'v2rayN',
+      icon: '',
+      introduction: '',
+      download: {
+        windows: 'https://github.com/2dust/v2rayN/releases/',
+        macOS: 'https://github.com/2dust/v2rayN/releases/'
+      }
+    },
+    {
+      label: 'Clash for Windows',
+      icon: '',
+      introduction: '',
+      download: {
+        windows: 'https://github.com/Fndroid/clash_for_windows_pkg/',
+        macOS: 'https://github.com/Fndroid/clash_for_windows_pkg/'
+      }
+    },
+    {
+      label: 'Clash for Windows 汉化补丁',
+      icon: '',
+      introduction: '',
+      download: {
+        windows: 'https://github.com/BoyceLig/Clash_Chinese_Patch/releases/',
+        macOS: 'https://github.com/BoyceLig/Clash_Chinese_Patch/releases/'
+      }
+    },
+    {
+      label: 'Snipaste',
+      icon: '',
+      introduction: '',
+      download: {
+        windows: 'https://dl.snipaste.com/win-x64-beta/',
+        macOS: 'https://dl.snipaste.com/mac-beta/'
+      }
+    },
+    {
+      label: 'Postman',
+      icon: '',
+      introduction: '',
+      download: {
+        windows: 'https://dl.pstmn.io/download/latest/win64/'
+      }
+    }
+  ],
+  Mandatory: [
+    {
+      label: 'VS Code',
+      icon: '',
+      introduction: 'Free. Built on open source. Runs everywhere.',
+      download: {
+        windows: 'https://code.visualstudio.com/docs/?dv=win64user',
+        macOS: 'https://code.visualstudio.com/docs/?dv=osx'
+      }
+    },
+    {
+      label: 'Git 64-bit',
+      icon: '',
+      introduction:
+        'Git is a free and open source distributed version control system.',
+      download: {
+        windows:
+          'https://github.com/git-for-windows/git/releases/download/v2.40.0.windows.1/Git-2.40.0-64-bit.exe',
+        macOS: 'https://git-scm.com/download/mac'
+      }
+    },
+    {
+      label: 'Chrome',
+      icon: '',
+      introduction: "There's no place like Chrome. Chrome is faster than fast",
+      download: {
+        windows:
+          'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B4520C673-308F-B754-8E0C-0E831E6867AE%7D%26lang%3Dzh-CN%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe',
+        macOS: 'https://www.google.cn/intl/zh-CN/chrome/'
+      }
+    }
+  ]
+}
